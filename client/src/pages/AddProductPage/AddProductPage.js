d7c30e7c28e6000cebba24615a7f997e423fcc6a
MatHyp <matijoker29@gmail.com>
2023-05-09 21:28:28 +0200

    Product page and other changes

--- client/src/pages/AddProductPage/AddProductPage.js
+++ client/src/pages/AddProductPage/AddProductPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
-
+import "AddProductPage.css";
 const AddProductPage = () => {
   return <div></div>;
 };
--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-
+import "./HomePage.css";
 const HomePage = () => {
+  const [products, setProducts] = useState([]);
   const getProducts = async (e) => {
     const response = await fetch(
       "http://localhost/Scandiweb-Junior-Developer-Test-Task/server/products",
@@ -11,8 +12,9 @@ const HomePage = () => {
       }
     );
 
-    const products = await response.json();
-    console.log(products);
+    const responseJson = await response.json();
+    console.log(responseJson);
+    setProducts(responseJson);
   };
 
   useEffect(() => {
@@ -21,7 +23,27 @@ const HomePage = () => {
 
   return (
     <div>
-      <div className="navbar"></div>
+      <div className="navbar">
+        <h1>Product List</h1>
+        <div className="buttons-contianer">
+          <button className="button button-add">ADD</button>
+          <button className="button button-remove" id="delete-product-btn">
+            MASS DELATE
+          </button>
+        </div>
+      </div>
+      {products.map((post) => {
+        return (
+          <div className="products-list">
+            <div className="product">
+              <input type="checkbox" className="delete-checkbox" />
+              <p>{post.sku}</p>
+              <p>{post.name}</p>
+              <p>{post.attribute}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
